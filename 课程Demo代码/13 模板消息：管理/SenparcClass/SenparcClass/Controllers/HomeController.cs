5c722e2fe077ee8a8e2ce51b2a28a9e0cd9ef36a
JeffreySu <www.jeffrey.su@gmail.com>
2018-02-08 23:24:08 +0800

    第13课初始代码

---
+++ 课程Demo代码/13 模板消息：管理/SenparcClass/SenparcClass/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using Senparc.Weixin.Cache;
+using Senparc.Weixin.Helpers.Extensions;
+using Senparc.Weixin.MP.Containers;
+using SenparcClass.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SenparcClass.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        private static int _count = 0;
+        public static int Count
+        {
+            get
+            {
+                Thread.Sleep(100);
+                return _count;
+            }
+            set
+            {
+                Thread.Sleep(100);
+                _count = value;
+            }
+        }
+
+
+        public ActionResult LockTest()
+        {
+            var strategy = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            using (strategy.BeginCacheLock("SenparcClass", "LockTest"))
+            {
+                var count = Count;
+                Thread.Sleep(2);
+                Count = count + 1;
+                return Content(count.ToString());
+            }
+        }
+
+        public ActionResult CustomMessage(string openId = "oxRg0uLsnpHjb8o93uVnwMK_WAVw")
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Thread.Sleep(500);
+                try
+                {
+                    Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendText(Config.AppId, openId, "从服务器发来的客服消息：" + (3 - i));
+                }
+                catch (Exception)
+                {
+                    //额外的一些处理
+                }
+
+            }
+
+            var result = Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendText(Config.AppId, openId, "从服务器发来的客服消息。" + DateTime.Now);
+
+            return Content(result.ToJson());
+        }
+
+        public async Task<ActionResult> CustomMessageAsync(string openId = "oxRg0uLsnpHjb8o93uVnwMK_WAVw")
+        {
+            return await Task.Factory.StartNew(async () =>
+             {
+                 for (int i = 0; i < 4; i++)
+                 {
+                     var result = await Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendTextAsync(Config.AppId, openId, "【异步方法】从服务器发来的客服消息。时间：{0}，编号：{1}".FormatWith(DateTime.Now.ToString("HH:mm:ss.ffff"), i + 1));
+                 }
+                 return Content("消息发送完毕！");
+             }).Result;
+        }
+
+        public async Task<ActionResult> CustomMessageSync(string openId = "oxRg0uLsnpHjb8o93uVnwMK_WAVw")
+        {
+            return await Task.Factory.StartNew(async () =>
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    //var result = await Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendTextAsync(Config.AppId, openId, "从服务器发来的客服消息。时间：{0}，编号：{1}".FormatWith(DateTime.Now, i + 1));
+                    var accessToken = AccessTokenContainer.GetAccessToken(Config.AppId);
+                    var result = Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendText(Config.AppId, openId, "【同步方法】从服务器发来的客服消息。时间：{0}，编号：{1}新AccessToken：{2}".FormatWith(DateTime.Now.ToString("HH:mm:ss.ffff"), i + 1, accessToken));
+                }
+                return Content("消息发送完毕！");
+            }).Result;
+        }
+
+        public async Task<ActionResult> ChangeAccessToken(string openId = "oxRg0uLsnpHjb8o93uVnwMK_WAVw")
+        {
+            return await Task.Factory.StartNew(async () =>
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    var accessToken = await AccessTokenContainer.GetAccessTokenAsync(Config.AppId, true);
+
+                    var result = await Senparc.Weixin.MP.AdvancedAPIs.CustomApi.SendTextAsync(accessToken, openId, "【异步方法】从服务器发来的客服消息。时间：{0}，编号：{1}\r\n\r\n新AccessToken：{2}".FormatWith(DateTime.Now.ToString("HH:mm:ss.ffff"), i + 1, accessToken));
+                }
+                return Content("消息发送完毕！");
+            }).Result;
+        }
+    }
+}
---
+++ 课程Demo代码/13 模板消息：管理/SenparcClass/SenparcClass/Controllers/RequestController.cs
@@ -0,0 +1,150 @@
+﻿using Senparc.Weixin.Exceptions;
+using Senparc.Weixin.Helpers.Extensions;
+using Senparc.Weixin.MP.Containers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SenparcClass.Controllers
+{
+    public class RequestController : Controller
+    {
+        public ActionResult Get(string url = "https://www.baidu.com")
+        {
+            var html = Senparc.Weixin.HttpUtility.RequestUtility.HttpGet(url, encoding: Encoding.GetEncoding("GB2312"));
+
+            html += "<script>alert('This is a remote page')</script>";
+
+            return Content(html);
+        }
+
+        public ActionResult SimulateLogin()
+        {
+            var url = "http://www.baidu.com";
+            var cookieContainer = new CookieContainer();
+            var html = Senparc.Weixin.HttpUtility.RequestUtility.HttpGet(url, cookieContainer, Encoding.UTF8, null, null, false);
+
+            return Content(html);
+        }
+
+        public ActionResult Post(string url = "https://sdk.weixin.senparc.com/AsyncMethods/TemplateMessageTest", string code = "")
+        {
+
+            var formData = new Dictionary<string, string>();
+            formData["checkcode"] = code;
+            var html = Senparc.Weixin.HttpUtility.RequestUtility.HttpPost(url, null, formData);
+
+            html += "<span sytle='color:red'>alert('it's a remote page by POST')</script>";
+
+            return Content(html);
+        }
+
+        public ActionResult GetImage(string url = "https://sdk.weixin.senparc.com/images/book-cover-front-small-3d-transparent.png")
+        {
+            var filePath = Server.MapPath("~/App_Data/");
+            //Server.MapPath("~/App_Data/DownloadImage_{0}.jpg".FormatWith(DateTime.Now.Ticks));
+            var fileName = Senparc.Weixin.HttpUtility.Get.Download(url, filePath);
+            var newFileName = fileName + ".png";
+            System.IO.File.Move(fileName, newFileName);
+
+            //Form 表单上传本地文件
+            var dic = new Dictionary<string, string>();
+            dic["file"] = newFileName;
+            var uploadUrl = "http://localhost:60716/Request/UploadFile";
+            //ServicePointManager.Expect100Continue = false;
+
+            var uploadResult = Senparc.Weixin.HttpUtility.RequestUtility.HttpPost(uploadUrl, fileDictionary: dic);
+
+            return Content("图片已保存到：" + fileName + "<br/>" + uploadResult);
+        }
+
+        public ActionResult GetAndUploadImage(string url = "https://sdk.weixin.senparc.com/images/book-cover-front-small-3d-transparent.png")
+        {
+            var fileName = Server.MapPath("~/App_Data/DownloadImage_{0}.jpg".FormatWith(DateTime.Now.Ticks));
+
+            using (var ms = new MemoryStream())
+            {
+                Senparc.Weixin.HttpUtility.Get.Download(url, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                //上传流
+                var uploadUrl = "http://localhost:60716/Request/UploadImage";
+                Senparc.Weixin.HttpUtility.RequestUtility.HttpPost(uploadUrl, null, ms);
+            }
+
+            return Content("图片已保存到：" + fileName);
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage()
+        {
+            var stream = Request.InputStream;
+            var fileName = Server.MapPath("~/App_Data/UploadImage.jpg");
+            using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                stream.CopyTo(fileStream);
+                stream.Flush();
+            }
+
+            return Content("文件已经下载、上传，保存到：" + fileName);
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            var stream = file.InputStream;
+            var fileName = Server.MapPath("~/App_Data/UploadFile.jpg");
+            using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                stream.CopyTo(fileStream);
+                stream.Flush();
+            }
+
+            return Content("文件已经下载、上传，保存到：" + fileName);
+        }
+
+        public class MyClass
+        {
+            public string Data { get; set; }
+        }
+
+
+        public ActionResult GetAccessToken() {
+
+            //var myClass = new MyClass();
+
+            //try
+            //{
+            //    if (myClass.Data == null)
+            //    {
+            //        throw new WeixinNullReferenceException("myClass.Data为null", myClass);
+            //    }
+            //}
+            //catch (WeixinNullReferenceException ex)
+            //{
+            //    if (true)
+            //    {
+            //        var obj = ex.ParentObject as MyClass;
+            //        obj.Data = "Not Null";
+
+            //        Senparc.Weixin.WeixinTrace.SendCustomLog("系统日志", ex.Message);
+            //    }
+            //}
+           
+            //var length = myClass.Data.Length;
+            //return Content("Length：" + length);
+
+            var accessToken = AccessTokenContainer.GetAccessToken(Service.Config.AppId+"invalid appid", true);
+
+            Senparc.Weixin.WeixinTrace.SendCustomLog("接口日志", "获取了新的AccessToken");
+
+            return Content("最新的AccessToken：" + accessToken+"<br />当前LogRecordCount："+Service.Config.LogRecordCount);
+        }
+
+    }
+}
