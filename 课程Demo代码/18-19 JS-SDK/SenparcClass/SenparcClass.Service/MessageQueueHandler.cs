f215e6796d5a8fa542409cea8050a17ea576cee4
JeffreySu <www.jeffrey.su@gmail.com>
2018-02-24 23:30:16 +0800

    添加18-19课课堂练习代码

---
+++ 课程Demo代码/18-19 JS-SDK/SenparcClass/SenparcClass.Service/MessageQueueHandler.cs
@@ -0,0 +1,54 @@
+﻿using Senparc.Weixin.MessageQueue;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenparcClass.Service
+{
+    public class MessageQueueHandler
+    {
+        public IResponseMessageBase SendMessage(string openId, IResponseMessageBase responseMessage)
+        {
+            var messageQueue = new SenparcMessageQueue();
+
+            if (responseMessage is ResponseMessageText)
+            {
+                {
+                    var mqKey = SenparcMessageQueue.GenerateKey("MessageHandlerSendMessageAsync", responseMessage.GetType(), Guid.NewGuid().ToString(), "SendMessage");
+                    messageQueue.Add(mqKey, () =>
+                    {
+                        var asyncResponseMessage = responseMessage as ResponseMessageText;
+                        asyncResponseMessage.Content += "\r\n\r\n - 这条消息来自客服接口-1";
+
+                        //发送客服消息
+                        CustomApi.SendText(Config.AppId, openId, asyncResponseMessage.Content);
+                    });
+                }
+                {
+                    var mqKey = SenparcMessageQueue.GenerateKey("MessageHandlerSendMessageAsync", responseMessage.GetType(), Guid.NewGuid().ToString(), "SendMessage");
+                    messageQueue.Add(mqKey, () =>
+                    {
+                        var asyncResponseMessage = responseMessage as ResponseMessageText;
+                        asyncResponseMessage.Content += "\r\n\r\n - 这条消息来自客服接口-2";
+
+                        //发送客服消息
+                        CustomApi.SendText(Config.AppId, openId, asyncResponseMessage.Content);
+                    });
+                }
+               
+               
+                return new ResponseMessageNoResponse();
+            }
+            //else if ()
+            //{
+            //    //...
+            //}
+
+            return responseMessage;
+        }
+    }
+}
