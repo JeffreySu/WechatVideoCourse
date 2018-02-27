005b6fc2fd994745af2136855aa055fe1b8c9184
JeffreySu <www.jeffrey.su@gmail.com>
2018-02-28 07:24:07 +0800

    19课代码

---
+++ 课程Demo代码/18-19 JS-SDK/SenparcClass/SenparcClass/Controllers/JSSDKController.cs
@@ -0,0 +1,53 @@
+﻿using Senparc.Weixin.MP.Helpers;
+using SenparcClass.Models.ViewData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SenparcClass.Controllers
+{
+    public class JSSDKController : OAuthBaseController
+    {
+        public JSSDKController()
+        {
+
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (ViewData.Model is Base_JSSDKVD)
+            {
+                var model = ViewData.Model as Base_JSSDKVD;
+
+                var jssdkUiPackage = JSSDKHelper.GetJsSdkUiPackage(Service.Config.AppId, Service.Config.AppSecret,
+              Request.Url.AbsoluteUri);
+
+                model.JsSdkUiPackage = jssdkUiPackage;
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+
+
+        public ActionResult Index()
+        {
+            //var jssdkUiPackage = JSSDKHelper.GetJsSdkUiPackage(Service.Config.AppId, Service.Config.AppSecret,
+            //    Request.Url.AbsoluteUri);
+
+            var vd = new JSSDK_Index()
+            {
+                //JsSdkUiPackage = jssdkUiPackage,
+                Msg = "当前用户信息：" + base.UserName
+            };
+
+            return View(vd);
+        }
+
+        //public ActionResult Action()
+        //{
+        //    return View();
+        //}
+    }
+}
--- 课程Demo代码/18-19 JS-SDK/SenparcClass/SenparcClass/Controllers/OAuthBaseController.cs
+++ 课程Demo代码/18-19 JS-SDK/SenparcClass/SenparcClass/Controllers/OAuthBaseController.cs
@@ -1,4 +1,5 @@
 ﻿using SenparcClass.Filters;
+using SenparcClass.Models.ViewData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,30 @@ namespace SenparcClass.Controllers
     [CustomSenparcOAuth(null, "/OAuth/Callback", Senparc.Weixin.MP.OAuthScope.snsapi_base)]
     public class OAuthBaseController : Controller
     {
+        public string UserName { get; set; }
+        public DateTime PageStartTime { get; set; }
 
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            var isLogined = User.Identity.IsAuthenticated;
+            if (true)
+            {
+                UserName = User.Identity.Name;
+            }
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+
+            if (ViewData.Model is OAuthBaseVD)
+            {
+                var model = ViewData.Model as OAuthBaseVD;
+                model.UserName = UserName;
+                model.PageRenderTime = DateTime.Now;
+            }
+        }
     }
 }
